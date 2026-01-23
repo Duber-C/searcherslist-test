126334828515bc3d119a1a9ba2c406abf864d363
lzuluagaSHV <lbernardo@shareholdervalue.com>
2026-01-22 21:43:12 -0500

    Deleted duplicate url in cors

--- searcher_api/settings.py
+++ searcher_api/settings.py
@@ -157,7 +157,6 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://searcherlist.com",
     "https://www.searcherlist.com",
 ]
 
